5e7a20d41bfc6f326966ad48ffba2d8ba6af3838
chenyiguo <iguochan@foxmail.com>
2022-10-26 22:14:52 +0800

    14 最长公共前缀

--- leetcode/editor/cn/14.LongestCommonPrefix_test.go
+++ leetcode/editor/cn/14.LongestCommonPrefix_test.go
@@ -42,7 +42,26 @@ func TestLongestCommonPrefix(t *testing.T) {
 
 //leetcode submit region begin(Prohibit modification and deletion)
 func longestCommonPrefix(strs []string) string {
+	l := len(strs)
+	if l == 0 {
+		return ""
+	}
+	if l == 1 {
+		return strs[0]
+	}
 
+	i := 0
+	for i = range strs[0] {
+		for j := 1; j < l; j++ {
+			// 如果任意比较子串的长度已经低于现在长度，则返回前子串
+			// 如果任意比较子串的字符不等于扫描到的字符，则返回前子串
+			if len(strs[j]) < i+1 || strs[0][i] != strs[j][i] {
+				return strs[0][:i]
+			}
+		}
+	}
+
+	return strs[0][:len(strs[0])]
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
